b9a0b2ced28d93f45da1e4553c359efbcfe019e9
FrankFrankWei <yanzhifei25@163.com>
2017-12-15 23:27:25 +0800

    Day 2 ViewData and ViewBag

---
+++ Demo/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo.Models;
+
+namespace Demo.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public ActionResult Index()
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Wei";
+            emp.LastName = "Frank";
+            emp.Salary = 20000f;
+
+            ViewData["Employee"] = emp;
+            return View("MyView");
+        }
+	}
+}
---
+++ Demo/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿/******************************************************************
+** auth: Frank
+** date: 12/15/2017 10:39:31 PM
+** desc:
+******************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class Employee
+    {
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public float Salary { set; get; }
+    }
+}
